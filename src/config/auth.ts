91362dbddf592271e1d3759e0eb553569f5c6254
Leo Nascimento <31623733+leonascimeto@users.noreply.github.com>
2021-12-16 00:12:51 -0300

    feat: update and upload for avatar image

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '894b3893b83fd1a26a80ed0d0b1473ba',
+    expiresIn: '1d',
+  },
+};
--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,11 +1,25 @@
 import { Router } from 'express';
 import { celebrate, Joi, Segments } from 'celebrate';
+import multer from 'multer';
+import uploadConfig from '@config/upload';
 import UserController from '../controllers/UserController';
+import isAuthenticated from '../../../shared/http/middlewares/isAuthenticated';
+import UserAvatarController from '../controllers/UserAvatarController';
 
 const usersRouter = Router();
 const usersController = new UserController();
+const usersAvatarController = new UserAvatarController();
 
-usersRouter.get('/', usersController.index);
+const upload = multer(uploadConfig);
+
+usersRouter.get('/', isAuthenticated, usersController.index);
+
+usersRouter.patch(
+  '/avatar',
+  isAuthenticated,
+  upload.single('avatar'),
+  usersAvatarController.update,
+);
 
 usersRouter.post(
   '/',
