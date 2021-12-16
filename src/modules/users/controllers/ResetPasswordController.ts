d7cb5e96ec1a5073310c7048cbca646848d99f8f
Leo Nascimento <31623733+leonascimeto@users.noreply.github.com>
2021-12-16 12:56:23 -0300

    feat: controller resetPassword

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import ResetPasswordService from '../services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { token, password } = req.body;
+
+    const resetPassword = new ResetPasswordService();
+
+    await resetPassword.execute({
+      password,
+      token,
+    });
+
+    return res.status(204).json();
+  }
+}
