410ee230edc6098e676a9f38164247c1ec7423df
Leo Nascimento <31623733+leonascimeto@users.noreply.github.com>
2021-12-16 12:50:59 -0300

    feat: routes forgotPassword

--- src/modules/users/controllers/ForgotPasswordController.ts
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
 
-export default class ForgotPassword {
+export default class ForgotPasswordController {
   public async create(req: Request, res: Response): Promise<Response> {
     const { email } = req.body;
 
